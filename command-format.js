39de33d193d6b5b4df03e1e095ba473c4b352a91
das_skyshadow <67155778+SkyShadowFlexX@users.noreply.github.com>
2022-05-30 16:49:48 +0200

    Update command-format.js

--- command-format.js
+++ command-format.js
@@ -1,34 +1,34 @@
 const Discord = require("discord.js");
-//Import Packages
+//Importiere Packages
 
 module.exports = {
-   name: "nameOfTheCommand",
-   //Name Of The Command
+   name: "name-cmd",
+   //Name von dem Command
    aliases: ["alias1", "alias2", "alias3"],
-   //Aliases For Command.
-   cooldowns: 1000, //1 second
-   //Cooldown For The Command [Milliseconds]
+   //Alias für den Command
+   cooldowns: 1000, //1000 = 1 Sekunde
+   //Cooldown für den Command (Millisekunden)
    description: "This Command Tells About You",
-   //Description Of The Command [The Purpose Etc...]
-   usage: "<user>",
-   //Usage For Command. [like ?nameOfTheCommand <user> <reason>]
+   //Beschreibung für den Command
+   usage: "<user> <reason>",
+   //Nutzung für den Command. [wie z.b ?name-cmd <user> <reason>]
    toggleOff: false,
-   //Disable The Command If Emergency. [true = off | false = on]
+   //Deaktiviere/Aktiviere den Command im Notfall. [true = inaktiv | false = aktiv]
    developersOnly: false,
-   //If Command Is Only For Bot Owners. [true = yes | false = no]
+   //Soll der Command nur für Developer sein. [true = ja | false = nein]
    /*
-    To Make Yourself Developer, Go Ahead to 
-    botconfig/main.json, set the ids in it. 
+    Um dich selber zum Dev zu machen gehe zu
+    botconfig/main.json, und setze die ID ein.
 */
    userpermissions: ["SEND_MESSAGES", "VIEW_CHANNEL"],
-   //Permissions Required For The Author To Use The CMD.
+   //Berechtigung die der Nutzer benötigt damit der Bot reagiert!
    botpermissions: ["ADMINISTRATOR"],
-   //Permissions Required For The Bot To Run The CMD.
+   //Berechtigungen die der Dev benötigt um Dev CMD's zu nutzen
 
    run: async (client, message, args) => {
       const member = message.mentions.members.first();
-      if (!member) return message.reply("Provide Some User To Tell About...");
+      if (!member) return message.reply("Nenne mir bitte einen Nutzer...");
 
-      message.reply(`About ${member}: \`He/She Looks Cool!\``);
+      message.reply(`About ${member}: \`Er/Sie nutzt SkyZ Hosting!\``);
    },
 };
